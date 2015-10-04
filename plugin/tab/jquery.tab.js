69081e8787a69a3a3ad7ff2ec98b3b501d1d6a5b
yinjiazeng <yinjiazeng@163.com>
2015-10-04 15:01:33 +0800

    选项卡

--- plugin/tab/jquery.tab.js
+++ plugin/tab/jquery.tab.js
@@ -1,36 +1,110 @@
-﻿$.fn.tab = function(o){
+﻿/**
+ * @fileName jquery.tab.js
+ * @author Aniu[date:2014-04-21 09:46]
+ * @update Aniu[date:2015-06-07 15:21]
+ * @version v1.3
+ * @description 选项卡
+ */
+
+$.fn.tab = function(o){
 	o = $.extend({
+		/**
+		 * @function 触发事件 click/mouseover
+		 * @type <String>
+		 */
 		event:'click',
+		/**
+		 * @function 是否自动切换 true/false
+		 * @type <Boolean>
+		 */
 		auto:false,
+		/**
+		 * @function 自动切换延迟时间
+		 * @type <Number>
+		 */
 		delay:4000,
+		/**
+		 * @function 默认展示菜单索引
+		 * @type <Number>
+		 */
 		index:0,
+		/**
+		 * @function 指定ajax数据容器元素，为空则指向最外层容器
+		 * @type <String>
+		 */
 		content:'',
+		/**
+		 * @function ajax请求数据
+		 * @type <Function>
+		 * @return <Undefined>
+		 * @param index <Number> 展示状态选项卡索引
+		 * @param item <jQuery Object> 展示内容区域容器对象
+		 */
 		ajax:$.noop,
-		callback:null
+		/**
+		 * @function 展示内容之前回调函数
+		 * @type <Function>
+		 * @return <Boolean>
+		 * @param index <Number> 展示状态选项卡索引
+		 * @param item <jQuery Object> 展示内容区域容器对象
+		 * @desc 返回false则不展示对应内容区域
+		 */
+		callback:$.noop,
+		/**
+		 * @function 展示内容之后回调函数
+		 * @type <Function>
+		 * @return <Undefined>
+		 * @param index <Number> 展示状态选项卡索引
+		 * @param item <jQuery Object> 展示内容区域容器对象
+		 */
+        endCallback:$.noop
 	}, o||{});
 	
 	return this.each(function(){
-		$(this).append('<div class="ui-tabody"></div>');
+        var _this = this, me = $(_this);
+		me.append('<div class="ui-tabody"></div>');
 		var index = 0, timer1 = timer2 = null,
-			that = $(this).children('.ui-tab').show(),
+			that = me.children('.ui-tab').show(),
 			size = that.children('li').size(),
-			body = $(this).children('.ui-tabody');
+			body = me.children('.ui-tabody');
+            
+        _this.addItem = function(data){
+            if($.isArray(data)){
+                var i = 0, len = data.length, temp, btn;
+                for(i; i<len; i++){
+                    temp = data[i];
+                    temp.tabmenu && (btn = $(temp.tabmenu).appendTo(that));
+                    temp.tabcon && $(temp.tabcon).appendTo(body).hide();
+                }
+                return btn;
+            }
+        }
+        
+        _this.removeItem = function(index, nextall){
+            if(!nextall){
+                that.children().eq(index).remove();
+                body.children().eq(index).remove();
+            }
+            else{
+                that.children().eq(index).nextAll().remove();
+                body.children().eq(index).nextAll().remove();
+            }
+        }
 		
 		that.children('li').children('a').siblings().appendTo(body).hide();
 		
-		that.children('li').each(function(i){
-			$(this).on(o.event, function(){
-				if(o.event == 'click'){
-					show(i);
-				}
-				else{
-					timer1 = setTimeout(function(){
-						show(i);
-					}, 150);
-				}
-				index = i;	
-			});
-		}).eq(o.index).trigger(o.event);
+        that.on(o.event, 'li', function(){
+            var i = $(this).index();
+            if(o.event == 'click'){
+                show(i);
+            }
+            else{
+                timer1 = setTimeout(function(){
+                    show(i);
+                }, 150);
+            }
+            index = i;	
+        }).children().eq(o.index).trigger(o.event);
 		
 		if(o.event === 'mouseover'){
 			that.on('mouseout', 'li', function(){
@@ -40,13 +114,14 @@
 		
 		function show(index){
 			var item = body.children(':eq(' + index + ')'), img = item.find('img'), content = o.content ? item.find(o.content).children()[0] : item.children()[0];
-			if(typeof o.callback === 'function' && o.callback(index, item) === false){
+			if(o.callback(index, item) === false){
                 return false;
             }
             !content && o.ajax(index, item);
 			(!!img.data('src') && (img.attr('src') !== img.data('src'))) && img.attr('src', img.data('src'));
 			that.children('li:eq(' + index + ')').addClass('s-crt').siblings().removeClass('s-crt');
 			item.show().siblings().hide();
+            o.endCallback(index, item);
 		} 
 
 		if(o.auto == true){
