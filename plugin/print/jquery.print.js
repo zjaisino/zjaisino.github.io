7f8dfcb04167c6e3f9a7b9d2b29c4cff31c36814
yinjiazeng <yinjiazeng@163.com>
2016-03-21 20:34:03 +0800

    增加打印插件

--- plugin/print/jquery.print.js
+++ plugin/print/jquery.print.js
@@ -1,13 +1,13 @@
 /**
  * @filename jquery.print.js
  * @author Aniu[2016-03-15 13:25]
- * @update Aniu[2016-03-15 13:25]
- * @version v1.1
+ * @update Aniu[2016-03-21 10:49]
+ * @version v1.2
  * @description 打印
  */
 
 $.fn.print = function(o){
-    o = $.extend({
+    o = $.extend(true, {
         /**
          * @func 打印元素
          * @type <Object, String>
@@ -15,11 +15,13 @@ $.fn.print = function(o){
          */
         element:'',
         /**
-         * @func 加载样式
-         * @type <Null, String, Array>
-         * @desc 若值不为null则使用iframe打印
+         * @func iframe打印
+         * @type <Object>
          */
-        cssurl:null,
+        iframe:{
+            enable:true,
+            title:''
+        },
         /**
          * @func 分页打印
          * @type <Function>
@@ -37,22 +39,50 @@ $.fn.print = function(o){
     //防止打印界面的该组件被再次调用
     if(!me.attr('bindprint')){
         me.attr('bindprint', '1');
+        var ele = typeof o.element === 'string' ? $(o.element).clone() : o.element.clone();
         me.on('click', function(e){
             if(o.element){
-                var ele = $(o.element).clone();
-                if(typeof o.cssurl === 'string'){
-                    o.cssurl = [o.cssurl];
-                }
-                if($.isArray(o.cssurl)){
-                    
+                if(o.iframe.enable === true){
+                    var iframeBox = $('<div><iframe /></div>').appendTo('body');
+                    var iframe = iframeBox.children();
+                    iframeBox.css({
+                        height:0,
+                        width:0,
+                        margin:0,
+                        padding:0,
+                        overflow:'hidden',
+                        dislay:'block'
+                    });
+                    var contents = iframe.contents();
+                    var iframeHead = contents.find('head');
+                    iframe = iframe[0];
+                    var iframeWin = iframe.contentWindow;
+                    var iframeDoc = iframeWin.document;
+                    iframeDoc.write('<title>'+ (o.iframe.title || '&nbsp;') +'</title>');
+                    o.callback(ele, page, iframeDoc);
+                    $('link[rel="stylesheet"]').each(function(){
+                        iframeDoc.write('<link rel="stylesheet" type="text/css" href="'+ $(this).attr('href') +'" />');
+                    });
+                    $('style').each(function(){
+                        iframeDoc.write('<style>'+ $(this).text() +'</style>');
+                    });
+                    iframeDoc.write(ele.prop('outerHTML'));
+                    iframeDoc.close();
+                    iframeWin.focus();
+                    setTimeout(function(){
+                        iframeWin.print();
+                        iframeBox.remove();
+                    }, 500);
                 }
                 else{
                     body.children().wrapAll('<div class="ui-printbox" style="display:none;"></div>');
                     o.callback(ele, page);
                     body.append(ele);
-                    print();
-                    ele.remove();
-                    $('.ui-printbox').children().unwrap();
+                    setTimeout(function(){
+                        print();
+                        ele.remove();
+                        $('.ui-printbox').children().unwrap();
+                    }, 100);
                 }
             }
             else{
